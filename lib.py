2b5f7711e89941e930a73e77336c293c0b43a63a
ShamailovMax <max24june@gmail.com>
2025-03-08 20:21:10 +0300

    added interface for future sqllib-funcs class

---
+++ lib.py
@@ -0,0 +1,21 @@
+# functions and keywords
+def select() -> str:
+    pass
+
+def from() -> str:
+    pass
+
+def where() -> str:
+    pass
+
+def group_by() -> str:
+    pass
+
+def order_by() -> str:
+    pass
+
+def having() -> str:
+    pass
+
+def over(partition_by: str) -> str:
+    pass
