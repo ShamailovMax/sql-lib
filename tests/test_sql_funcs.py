1a609a59281b5b3b9a433cadcdc0fc6fc8ed210b
ShamailovMax <max24june@gmail.com>
2025-03-11 00:35:19 +0300

    added first testcases

--- tests/test_sql_funcs.py
+++ tests/test_sql_funcs.py
@@ -1,6 +1,22 @@
-# Новый корректный импорт
+import unittest
 from core.sql_funcs import SQLLibFunctions
 
-# Пример использования
-qq = SQLLibFunctions()
-qq.select(['*']).build()
+# select method test
+class TestSelectMethod(unittest.TestCase):
+    # created setUp method not to repeat my code
+    def setUp(self):
+        self.sql_builder = SQLLibFunctions()
+        
+    def test_select_single_field(self):
+        result = self.sql_builder.select(['name'])
+        self.assertEqual(result.query, 'select name \n')
+        self.assertIsInstance(result, SQLLibFunctions)
+        
+    def test_select_multiple_fields(self):
+        result = self.sql_builder.select(['name', 'age', 'email'])
+        self.assertEqual(result.query, 'select name, age, email \n')
+        self.assertIsInstance(result, SQLLibFunctions)
+        
+    
+if __name__ == "__main__":
+    unittest.main()
